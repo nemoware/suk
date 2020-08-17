1f328cf10840463852873606d67f2fdeae7243bf
Sergey Shalaevsky <shalaevskiy@nemosoft.ru>
2020-08-17 12:25:40 +0300

    removed: unnecessary code

--- suk_ui/SessionState.py
+++ suk_ui/SessionState.py
@@ -75,29 +75,6 @@ def get(**kwargs):
     >>> session_state.user_name
     'Mary'
     """
-    # Hack to get the session object from Streamlit.
-
-    ctx = ReportThread.get_report_ctx()
-
-    this_session = None
-
-    current_server = Server.get_current()
-    if hasattr(current_server, '_session_infos'):
-        # Streamlit < 0.56
-        session_infos = Server.get_current()._session_infos.values()
-    else:
-        session_infos = Server.get_current()._session_info_by_id.values()
-
-    # for session_info in session_infos:
-    #     s = session_info.session
-    #     if (
-    #             # Streamlit < 0.54.0
-    #             (hasattr(s, '_main_dg') and s._main_dg == ctx.main_dg)
-    #             or
-    #             # Streamlit >= 0.54.0
-    #             (not hasattr(s, '_main_dg') and s.enqueue == ctx.enqueue)
-    #     ):
-    #         this_session = s
     this_session = _get_session()
 
     if this_session is None:
